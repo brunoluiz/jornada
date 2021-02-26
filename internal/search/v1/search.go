3fc6cb53e86ea19868f082a5625a6e2d13bdc5a4
Bruno Luiz Silva <contact@brunoluiz.net>
2021-02-26 16:09:19 +0000

    judge me, I am implementing this search with regex

---
+++ internal/search/v1/search.go
@@ -0,0 +1,49 @@
+package search
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var metaRegex = regexp.MustCompile(`(meta\.[\w]+)([\s=])+([\d]|['\w])+`)
+var quotesRegex = regexp.MustCompile(`'(.*?)'`)
+var validRegex = regexp.MustCompile(`^([\w\d\s.='"()><=!/-])+$`)
+var dangerousRegex = regexp.MustCompile(`((--)|([/*])).+`)
+
+// ToSQL parse an input string to a valid SQL string
+// TODO: It should be replaced with a v2, with a proper DSL (which I am not currently bothered atm)
+func ToSQL(in string) (out string, params []interface{}, err error) {
+	if ok := validRegex.MatchString(in); !ok {
+		return out, params, errors.New("Invalid query")
+	}
+
+	// Replace all " with '
+	out = strings.ReplaceAll(in, "\"", "'")
+
+	// Replace all possibily dangerous expressions
+	out = dangerousRegex.ReplaceAllString(in, "")
+
+	// Transform all `meta.foo = 'bar'` into `meta.key = 'foo' and meta.value = 'bar'`
+	out = metaRegex.ReplaceAllStringFunc(out, func(m string) string {
+		// strip quote mark
+		m = strings.Replace(m, "'", "", -1)
+		entries := strings.Split(m, "=")
+
+		ident, value := strings.Split(entries[0], "."), entries[1]
+		key := ident[1]
+
+		return fmt.Sprintf("(meta.key = '%s' AND meta.value = '%s')", key, value)
+	})
+
+	// Print all strings as SQL placeholders
+	out = quotesRegex.ReplaceAllStringFunc(out, func(m string) string {
+		m = strings.Replace(m, "'", "", -1)
+		m = strings.Trim(m, " ")
+		params = append(params, m)
+		return fmt.Sprintf("$%d", len(params))
+	})
+
+	return strings.Trim(out, " "), params, nil
+}
--- internal/search/v1/search_test.go
+++ internal/search/v1/search_test.go
@@ -3,7 +3,7 @@ package search_test
 import (
 	"testing"
 
-	"github.com/brunoluiz/jornada/internal/search"
+	"github.com/brunoluiz/jornada/internal/search/v1"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,6 +12,7 @@ func TestTransform(t *testing.T) {
 		in     string
 		out    string
 		params []interface{}
+		err    bool
 	}{
 		{
 			in:     "meta.foo = 'bar' AND meta.x = 'y'",
@@ -19,21 +20,30 @@ func TestTransform(t *testing.T) {
 			params: []interface{}{"foo", "bar", "x", "y"},
 		},
 		{
-			in:     "meta.foo = ';;bar' AND meta.x = 'y'",
-			out:    "(meta.key = $1 AND meta.value = $2) AND (meta.key = $3 AND meta.value = $4)",
-			params: []interface{}{"foo", "bar", "x", "y"},
+			in:  "meta.foo = ';;bar' AND meta.x = 'y'",
+			err: true,
 		},
 		{
 			in:     "(meta.foo = 'bar' AND meta.x = 'y') OR value = '1'",
 			out:    "((meta.key = $1 AND meta.value = $2) AND (meta.key = $3 AND meta.value = $4)) OR value = $5",
 			params: []interface{}{"foo", "bar", "x", "y", "1"},
 		},
+		{
+			in:     "(meta.foo = 10 AND meta.x = 'y') OR value = '1'",
+			out:    "((meta.key = $1 AND meta.value = $2) AND (meta.key = $3 AND meta.value = $4)) OR value = $5",
+			params: []interface{}{"foo", "10", "x", "y", "1"},
+		},
+		{
+			in:     "meta.test = 'x' -- comment",
+			out:    "(meta.key = $1 AND meta.value = $2)",
+			params: []interface{}{"test", "x"},
+		},
 	}
 
 	for _, test := range tests {
-		out, params, err := search.Transform(test.in)
-		require.NoError(t, err)
-		require.Equal(t, out, test.out)
-		require.Equal(t, params, test.params)
+		out, params, err := search.ToSQL(test.in)
+		require.Equal(t, test.err, err != nil)
+		require.Equal(t, test.out, out)
+		require.Equal(t, test.params, params)
 	}
 }
